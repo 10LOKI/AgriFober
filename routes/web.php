9ed65c774e8cfc61806a55607c80898b905b66da
IUB_LOKI <ayoubouharda33@gmail.com>
2026-05-13 16:40:04 +0100

    Ajout des routes dans le web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\UserController;
+use App\Http\Controllers\Admin\CultureController as AdminCultureController;
+use App\Http\Controllers\Admin\ProductController as AdminProductController;
+use App\Http\Controllers\Admin\RegionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::middleware(['web', 'auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+    // Users
+    Route::resource('users', UserController::class);
+    Route::post('users/{user}/approve', [UserController::class, 'approve'])->name('users.approve');
+    Route::post('users/{user}/reject', [UserController::class, 'reject'])->name('users.reject');
+
+    // Cultures
+    Route::resource('cultures', AdminCultureController::class);
+
+    // Products
+    Route::resource('products', AdminProductController::class);
+
+    // Regions
+    Route::resource('regions', RegionController::class)->except(['show']);
 });
